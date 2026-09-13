fbadd02f1b7fbc7023dd91a1e0e2a8f193d63385
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-13 15:52:53 +0200

    Update plugin.py

--- usr/lib/enigma2/python/Plugins/Extensions/CrashlogViewer/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/CrashlogViewer/plugin.py
@@ -57,7 +57,7 @@ LOG_BASE_PATH = "/home/root/logs/"
 # Fallback-Version.
 # Die tatsächlich installierte Version wird bevorzugt aus
 # version.txt gelesen.
-version = "2.5.1"
+version = "2.5.2"
 
 LOGFILE = "/tmp/CrashlogViewer.log"
 UPDATE_LOGFILE = "/tmp/CrashlogViewer-update.log"
